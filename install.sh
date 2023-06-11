315a2a85e26cb1edeba8eca2bcec3d68602a4190
Logicer <81077770+Logicer16@users.noreply.github.com>
2023-06-11 22:55:30 +1000

    Create install.sh
    closes #1

---
+++ install.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+set -e # -e: exit on error
+
+print_usage() {
+  return "`basename $0`:usage: [-s|-l] [-h]
+Install source:
+Default: decided automatically 
+  -c, --clone: clone from GitHub
+  -l, --local: install from the script's directory"
+}
+
+# Get args
+# Compounded options (i.e. -abc) would require a special case
+for i in $@; do
+  arg=`echo $i | tr "[:lower:]"`
+  case $arg in
+
+   -c | --clone )
+      if [ ! -z $arg_source ]; then
+        echo "error: multiple install source statements" >&2
+        exit 1
+      fi
+      arg_source=clone
+      ;;
+
+   -l | --local )
+      if [ ! -z $arg_source ]; then
+        echo "error: multiple install source statements" >&2
+        exit 1
+      fi
+      arg_source=local
+      ;;
+
+   -h)
+      echo "POSIX install script for logicer's dotfiles.\nLearn more: https://github.com/Logicer16/dotfiles\n"
+      echo `usage()`
+      exit 0
+      ;;
+
+   *)  
+      echo `usage()` >&2
+      exit 1
+      ;;
+
+  esac
+done
+
+# Download chezmoi for bootstraping if required
+if [ ! "`command -v chezmoi`" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "`command -v curl`" ]; then
+    sh -c "`curl -fsSL https://git.io/chezmoi`" -- -b "$bin_dir"
+  elif [ "`command -v wget`" ]; then
+    sh -c "`wget -qO- https://git.io/chezmoi`" -- -b "$bin_dir"
+  else
+    echo "error: cannot install chezmoi. You must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
+
+# Decide source
+if $arg_source; then
+  # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+  # Looks like garbage but blame the bourne shell's syntax for not having $()
+  script_dir="`cd -P -- \"\`dirname -- \\\"\\\`command -v -- \\\\\"$0\\\\\"\\\`\\\"\`\" && pwd -P`"
+  echo `$chezmoi git -S $script_dir -- config --get remote.origin.url`
+  if [[ `$chezmoi git -S $script_dir -- config --get remote.origin.url` == "https://github.com/Logicer16/dotfiles.git" ]] && [[ $arg_source != "clone" ]]; then
+    # update git repo
+    $chezmoi git -S $script_dir -- pull
+    source="--source=$script_dir"
+  else
+    if [[ $arg_source == "local" ]]; then
+      echo "error: not in git repository" >&2
+      exit 1
+    fi
+  fi
+fi
+
+# Pass unattended status
+if [ ! -t 0 ] || [ ! -t 1 ]; then
+  UNATTENDED=true
+fi
+
+# exec: replace current process with chezmoi init
+exec env UNATTENDED=$UNATTENDED "$chezmoi" init --apply ${source:-"logicer16"}
